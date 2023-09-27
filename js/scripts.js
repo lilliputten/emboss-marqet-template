e05e6390386f3ce55416b1070bda682cad47dd29
lilliputten <lilliputten@yandex.ru>
2023-09-27 17:08:09 +0700

    Build publish.0.0.1, 2023.09.27, 13:07 GMT+3 (230927-1307)

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+// TODO: Use traspiller, see https://www.raresportan.com/eleventy-part-five/
+
+// See also: `src/static/vendor/js/script.js`
+
+var loaded = false;
+
+function onLoad() {
+  // console.log('[scripts:onLoad]');
+  if (!loaded) {
+    loaded = true;
+    window.removeEventListener('load', onLoad);
+    var body = document.body;
+    body.classList.add('loaded');
+  }
+}
+
+function onDebugCustomReload() {
+  // console.log('[scripts:onDebugCustomReload]');
+  onLoad();
+}
+
+function started() {
+  // NOTE: Beware of `eleventy.reload` event.
+  // See `node_modules/@11ty/eleventy-dev-server/client/reload-client.js`
+  /* console.log('[scripts:started]', {
+   *   loaded,
+   * });
+   */
+  var body = document.body;
+  body.classList.add('inited');
+  body.classList.remove('noscript');
+}
+
+window.addEventListener('load', onLoad);
+
+// DEBUG: 11thy live-reload handler...
+(window.__onDebugReload = window.__onDebugReload || []).push(onDebugCustomReload);
+
+started();
