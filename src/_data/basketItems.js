ceefc14808722de2d3a041b0e859f759fd198860
lilliputten <lilliputten@yandex.ru>
2023-12-24 20:45:21 +0700

    Issue #15: Added profile order page, wishlist page, refactored basket (shopping cart) page, orders list page.

---
+++ src/_data/basketItems.js
@@ -0,0 +1,74 @@
+// @ts-check
+
+/** @type TBasketItem[] */
+const basketItems = [
+  /*
+  {
+    image: '/vendor/images/products/products-modelist-1/product01.png',
+    priceOld: 2500,
+    price: 1450,
+    // name: 'consectetur adipiscing elit',
+    name: 'Looking lorem round, to ascertain that they - extra_long_text_line_for_testing_purposes_only',
+    id: 4564789,
+    count: 1,
+  },
+  */
+  {
+    image: '/vendor/images/products/products-modelist-1/product02.png',
+    // priceOld: 350,
+    price: 245,
+    name: 'Man sport Hiking shoe',
+    id: 389645,
+    count: 2,
+  },
+  {
+    image: '/vendor/images/products/products-modelist-1/product03.png',
+    priceOld: 450,
+    price: 245,
+    name: 'Woman highheel shoe',
+    id: 8464759,
+    count: 1,
+  },
+  {
+    image: '/vendor/images/products/products-modelist-1/product04.png',
+    priceOld: 120,
+    price: 95,
+    name: 'Woman highheel shoe',
+    id: 83925602,
+    count: 1,
+  },
+  /*
+  {
+    image: '/vendor/images/products/products-modelist-1/product03.png',
+    priceOld: 450,
+    price: 245,
+    name: 'Woman highheel shoe',
+    id: 82510691,
+    count: 3,
+  },
+  {
+    image: '/vendor/images/products/products-modelist-1/product02.png',
+    price: 245,
+    name: 'Man sport Hiking shoe',
+    id: 12963715,
+    count: 1,
+  },
+  {
+    image: '/vendor/images/products/products-modelist-1/product04.png',
+    price: 95,
+    name: 'colorful cool runnig shoes',
+    id: 98159203,
+    count: 1,
+  },
+  {
+    image: '/vendor/images/products/products-modelist-1/product01.png',
+    price: 145,
+    name: 'consectetur adipiscing elit',
+    id: 5278539,
+    count: 1,
+  },
+  */
+];
+
+module.exports = basketItems;
+
