11332b4330577a7d88a5ccd1f2691094c3632caf
lilliputten <lilliputten@yandex.ru>
2024-01-12 00:08:40 +0700

    Issue #17: First part of adaptive fixups (fixed tables).

--- src/css/page.11ty.js
+++ src/css/page.11ty.js
@@ -14,22 +14,43 @@ module.exports = class {
     };
   }
 
+  // TODO: Add common variables?
   async render({ rawCss, rawFilepath }) {
     return await postcss([
-      // https://www.npmjs.com/package/postcss-strip-inline-comments -- ???
-      require('postcss-strip-inline-comments'),
-      require('postcss-mixins'),
-      require('postcss-simple-vars'),
       require('postcss-import'),
+      // require('postcss-strip-inline-comments'), // https://www.npmjs.com/package/postcss-strip-inline-comments -- ???
+      require('postcss-mixins'),
+      require('postcss-simple-vars')({
+        // @see https://github.com/postcss/postcss-simple-vars#options
+        variables: require('./global-variables'),
+        /* // DEBUG: Global css variables
+         * onVariables(variables) {
+         *   console.log('CSS Variables');
+         *   console.log(JSON.stringify(variables, null, 2));
+         * },
+         */
+      }),
+      // require('postcss-media-variables'), // 1st call // @see https://github.com/WolfgangKluge/postcss-media-variables
+      // require('postcss-custom-media'),
+      // require('postcss-custom-properties'),
+      // require('postcss-css-variables'), // Variables work for 'postcss-media-variables' but don't wqork in css body
+      require('postcss-calc')({ mediaQueries: true }),
+      // require('postcss-media-variables'), // 2nd call
       // require('tailwindcss/nesting'),
-      // @see https://tailwindcss.com/docs/using-with-preprocessors
       require('postcss-nested'),
       // require('postcss-nesting'),
       require('autoprefixer'),
-      require('tailwindcss'),
-      // require('cssnano'),
+      require('tailwindcss'), // @see https://tailwindcss.com/docs/using-with-preprocessors
+      // require('cssnano'), // Minimize css (TODO: Do it optionally?)
     ])
-      .process(rawCss, { from: rawFilepath })
+      .process(rawCss, {
+        from: rawFilepath,
+        parser: require('postcss-scss'),
+        map: {
+          // inline: false,
+          // annotation: true,
+        },
+      })
       .then((result) => result.css);
   }
 };
